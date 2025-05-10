9b65f29c304d4c509f54004ede1ae5705089ae44
mustafa sayed <mustafasayed123m@gmail.com>
2025-05-10 17:39:51 +0300

    Add files via upload

---
+++ todo_management/__manifest__.py
@@ -0,0 +1,13 @@
+{
+     'name': "Todo Task",
+     'author': "Mustafa Sayed",
+     'category': '',
+     'version': '17.0.0.1.0',
+     'depends': ['base','mail'],
+     'data': [
+         'security/ir.model.access.csv',
+         'views/base_menu.xml',
+         'views\Todo_Task.xml',
+     ],
+     'application': True,
+ }
